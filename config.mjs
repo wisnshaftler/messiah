3a7c58696bd2148920f95910f5e6d9898b1feb3c
wisnshaftler <wisnshaftler@gmail.com>
2023-09-30 22:57:19 +0530

    better use nextjs

--- config.mjs
+++
@@ -1,7 +0,0 @@
-import *  as dotenv from "dotenv";
-dotenv.config();
-
-export function config() {
-    admin_username = process.env.ADMIN_USERNAME,
-    admin_password = process.env.ADMIN_PASSWORD
-}
